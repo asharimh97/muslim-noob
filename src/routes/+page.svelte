89c018d0869b9c782c1e6576acd52ba79fa9ed53
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2025-07-05 23:01:07 +0700

    feat: add supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>SvelteKit</title>
+	<meta name="description" content="SvelteKit" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="icon" href="/favicon.svg" />
+</svelte:head>
+
+<div class="flex flex-col gap-4">
+	<div class="flex flex-col gap-2">
+		<h2 class="text-lg font-bold">Welcome to SvelteKit</h2>
+		<p>
+			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+		</p>
+	</div>
+</div>
