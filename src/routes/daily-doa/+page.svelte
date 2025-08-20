0d7c2697fcfc24b8cf74215250b08c70a80e5770
Ashari Muhammad H <asharimh97@gmail.com>
2025-08-20 14:59:37 +0700

    feat: add doa pages

---
+++ src/routes/daily-doa/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MetaTag from '$lib/components/chores/MetaTag.svelte';
+</script>
+
+<svelte:head>
+	<MetaTag />
+</svelte:head>
+
+<p>Hadits of the day</p>
---
+++ src/routes/doa-of-the-day/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MetaTag from '$lib/components/chores/MetaTag.svelte';
+</script>
+
+<svelte:head>
+	<MetaTag />
+</svelte:head>
+
+<p>Doa of the day</p>
