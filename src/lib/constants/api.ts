a4d982f75aba9b8076ca14a3138fa5c00d0290b0
Ashari Muhammad H <asharimh97@gmail.com>
2025-08-20 14:59:24 +0700

    chore: add constants

---
+++ src/lib/constants/api.ts
@@ -0,0 +1,3 @@
+export const HADITS_API_URL = 'https://hadithapi.com/api/';
+export const ALQURAN_API_URL = 'https://api.alquran.cloud/v1/';
+export const EQURAN_API_URL = 'https://equran.id/api/v2/';
